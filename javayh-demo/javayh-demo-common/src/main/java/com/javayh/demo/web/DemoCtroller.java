ab8c59399f5d62677243244dfdfac628c70d1ae9
haiji.yang <haiji.yang@capgemini.com>
2020-03-05 15:33:29 +0800

    add rsa

--- javayh-demo/javayh-demo-common/src/main/java/com/javayh/demo/web/DemoCtroller.java
+++ javayh-demo/javayh-demo-common/src/main/java/com/javayh/demo/web/DemoCtroller.java
@@ -1,14 +1,21 @@
 package com.javayh.demo.web;
 
+import com.javayh.common.encrypt.RSAEncrypt;
+import com.javayh.common.encrypt.RSAUtil;
 import com.javayh.common.result.ResultData;
 import com.javayh.demo.service.DemoService;
 import com.javayh.log.annotation.SysLog;
-import com.javayh.redis.prefix.KeyUtils;
-import com.javayh.redis.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Map;
+
+import static com.javayh.common.encrypt.RSAEncrypt.initKey;
 
 /**
  * <p>
@@ -26,8 +33,8 @@ public class DemoCtroller {
     @Autowired
     private DemoService demoService;
 
-    @Autowired
-    private RedisUtil redisUtil;
+//    @Autowired
+//    private RedisUtil redisUtil;
 
     /**
      * <p>
@@ -90,11 +97,35 @@ public class DemoCtroller {
      * @return com.javayh.common.result.ResultData
      */
 //    @SysLog(value = "javayh-demo-common",detail = "测试Redis")
-    @GetMapping(value = "redis")
-    public ResultData redis(){
-        String ceshi = KeyUtils.key("ceshi");
-        boolean hello_word = redisUtil.setl(ceshi,  "hello word", 100);
-        String s = (String) redisUtil.get(ceshi);
-        return ResultData.success(s);
+//    @GetMapping(value = "redis")
+//    public ResultData redis(){
+//        String ceshi = KeyUtils.key("ceshi");
+//        boolean hello_word = redisUtil.setl(ceshi,  "hello word", 100);
+//        String s = (String) redisUtil.get(ceshi);
+//        return ResultData.success(s);
+//    }
+
+
+    /**
+     * <p>
+     *       测试加密解密
+     * </p>
+     * @version 1.0.0
+     * @author Dylan-haiji
+     * @since 2020/3/5
+     * @param key
+     * @return com.javayh.common.result.ResultData
+     */
+    @GetMapping(value = "rsa/{key}")
+    public ResultData getE(@PathVariable String key) throws Exception {
+        Map<String, Object>  keyMap = initKey();
+        PublicKey publicKey = RSAUtil.getPublicKey(RSAEncrypt.getPublicKey(keyMap));
+        System.out.println(publicKey);
+        PrivateKey privateKey =  RSAUtil.getPrivateKey(RSAEncrypt.getPrivateKey(keyMap));
+        System.out.println(privateKey);
+        String source = RSAUtil.encryptString(publicKey, key);
+        String oldSource = RSAUtil.decryptString(privateKey, source);
+        System.out.println("解密后数据:" + oldSource);
+        return ResultData.success(oldSource);
     }
 }
---
+++ javayh-starter/javayh-common-starter/src/main/java/com/javayh/common/encrypt/RSAEncrypt.java
@@ -0,0 +1,104 @@
+package com.javayh.common.encrypt;
+
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *       生成公钥私钥
+ * </p>
+ * @version 1.0.0
+ * @author zhao-baolin
+ * @since 2020/3/5
+ */
+public class RSAEncrypt {
+
+    public static final String KEY_ALGORITHM = "RSA";
+    private static final String PUBLIC_KEY = "RSAPublicKey";
+    private static final String PRIVATE_KEY = "RSAPrivateKey";
+
+    public class Keys {
+
+    }
+
+    //获得公钥
+    public static String getPublicKey(Map<String, Object> keyMap) throws Exception {
+        //获得map中的公钥对象 转为key对象
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        //byte[] publicKey = key.getEncoded();
+        //编码返回字符串
+        return encryptBASE64(key.getEncoded());
+    }
+
+    //获得私钥
+    public static String getPrivateKey(Map<String, Object> keyMap) throws Exception {
+        //获得map中的私钥对象 转为key对象
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        //byte[] privateKey = key.getEncoded();
+        //编码返回字符串
+        return encryptBASE64(key.getEncoded());
+    }
+
+    /**
+     * <p>
+     *      编码返回字符串
+     * </p>
+     * @version 1.0.0
+     * @author Dylan-haiji
+     * @since 2020/3/5
+     * @param key
+     * @return java.lang.String
+     */
+    public static String encryptBASE64(byte[] key) throws Exception {
+        return Base64.encodeBase64String(key);
+    }
+
+    /**
+     * <p>
+     *       map对象中存放公私钥
+     * </p>
+     * @version 1.0.0
+     * @author Dylan-haiji
+     * @since 2020/3/5
+     * @param
+     * @return java.util.Map<java.lang.String,java.lang.Object>
+     */
+    public static Map<String, Object> initKey() throws Exception {
+        //获得对象 KeyPairGenerator 参数 RSA 256个字节
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        keyPairGen.initialize(1024);
+        //通过对象 KeyPairGenerator 获取对象KeyPair
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        //通过对象 KeyPair 获取RSA公私钥对象RSAPublicKey RSAPrivateKey
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        //公私钥对象存入map中
+        Map<String, Object> keyMap = new HashMap<String, Object>(2);
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+        return keyMap;
+    }
+
+
+//    public static void main(String[] args) {
+//        Map<String, Object> keyMap;
+//        try {
+//            keyMap = initKey();
+//            String publicKey = getPublicKey(keyMap);
+//            System.out.println(publicKey);
+//            String privateKey = getPrivateKey(keyMap);
+//            System.out.println(privateKey);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
