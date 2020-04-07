b832b3828cd1ba7ebb701ff031a29f9a4ce958dc
haiji.yang <haiji.yang@capgemini.com>
2020-04-07 18:36:04 +0800

    update oauth

--- doc/sql/sso/oauth.sql
+++ doc/sql/sso/oauth.sql
@@ -1,18 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 47.105.196.42
- Source Server Type    : MySQL
- Source Server Version : 50728
- Source Host           : 47.105.196.42:3306
- Source Schema         : oauth2
-
- Target Server Type    : MySQL
- Target Server Version : 50728
- File Encoding         : 65001
-
- Date: 06/04/2020 13:26:08
-*/
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
--- javayh-dependencies/javayh-mybatis-starter/src/main/java/com/javayh/mybatis/page/PageEntity.java
+++ javayh-dependencies/javayh-mybatis-starter/src/main/java/com/javayh/mybatis/page/PageEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * <p>
@@ -14,14 +15,50 @@ import lombok.Setter;
  * @version 1.0.0
  * @since 2020-04-04 22:41
  */
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class PageEntity {
-
+    /** 起始页*/
 	private int pageNo = 1;
-
+    /**每页的条数*/
 	private int pageSize;
+	/**排序的字段*/
+	private String orderBy;
 
+    public PageEntity() {
+    }
+
+    public PageEntity(int pageNo, int pageSize, String orderBy) {
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+        this.orderBy = orderBy;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        if(orderBy.contains("-")){
+            orderBy=orderBy.replaceAll("-"," desc");
+
+        }else if (orderBy.contains("+")){
+            orderBy = orderBy.replaceAll("'+'", " asc");
+        }
+        this.orderBy = orderBy;
+    }
 }
