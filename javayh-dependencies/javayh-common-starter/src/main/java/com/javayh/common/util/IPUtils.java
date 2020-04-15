9a6d11fb0d8aab3fcef01890d7c65348b4a4bb56
yanghaiji <yanghaij327@163.com>
2020-04-15 16:24:20 +0800

    update jdk version 8 -> 14

--- javayh-dependencies/javayh-common-starter/src/main/java/com/javayh/common/util/IPUtils.java
+++ javayh-dependencies/javayh-common-starter/src/main/java/com/javayh/common/util/IPUtils.java
@@ -24,16 +24,13 @@ public class IPUtils {
 			Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface
 					.getNetworkInterfaces();
 			while (allNetInterfaces.hasMoreElements()) {
-				NetworkInterface netInterface = (NetworkInterface) allNetInterfaces
+				NetworkInterface netInterface = allNetInterfaces
 						.nextElement();
 				Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
 				while (addresses.hasMoreElements()) {
-					InetAddress ip = (InetAddress) addresses.nextElement();
-					if (ip != null && ip instanceof Inet4Address
-							&& !ip.isLoopbackAddress() // loopback地址即本机地址，IPv4的loopback范围是127.0.0.0
-														// ~ 127.255.255.255
-							&& ip.getHostAddress().indexOf(":") == -1) {
-						return ip.getHostAddress();
+					InetAddress ip = addresses.nextElement();
+					if (ip instanceof Inet4Address hostIp && !ip.isLoopbackAddress() && !ip.getHostAddress().contains(":")) {
+						return hostIp.getHostAddress();
 					}
 				}
 			}
