9b50adeb4298da9cd17e55ad684c32ec81de65eb
haiji.yang <haiji.yang@capgemini.com>
2020-05-07 09:13:52 +0800

    add java-file

---
+++ javayh-demo/javayh-demo-file/src/main/java/com/javayh/file/FileApplication.java
@@ -0,0 +1,21 @@
+package com.javayh.file;
+
+import com.javayh.common.annotation.JavayhBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * <p>
+ * file 启动类
+ * </p>
+ *
+ * @author Dylan-haiji
+ * @version 1.0.0
+ * @since 2020-05-07
+ */
+@JavayhBootApplication
+public class FileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class,args);
+    }
+}
